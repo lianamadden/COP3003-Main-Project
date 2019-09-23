3f5a53ab415a46492fc9a785449eef5227306b01
liana <lianamadden@ymail.com>
2019-09-23 17:33:54 -0400

    Got the Database to connect!

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,20 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+
 public class Controller {
+
+    @FXML
+    Button button1 = new Button();
+
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        // Button was clicked, do something...
+        System.out.println("You pressed the button!");
+
+    }
 }
